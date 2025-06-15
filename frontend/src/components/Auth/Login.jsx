72ba90b3b0de8391b2912b0838374b6a20eabc41
THE Rock43 <ak9072374@gmail.com>
2025-06-15 11:44:01 +0530

    changes

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        `https://job-portal-itt5.vercel.app/api/v1/user/login`,
+        `${process.env.REACT_APP_API_URL}/api/v1/user/login`,
         { email, password, role },
         {
           headers: {
