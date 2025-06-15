838a4421ac9bd04ea0790d4e55f4d219bcc5fb2b
THE Rock43 <ak9072374@gmail.com>
2025-06-15 12:28:41 +0530

    changes

--- backend/api/index.js
+++
@@ -1,8 +0,0 @@
-import serverless from "serverless-http";
-import app from "../app.js";
-import dbConnection from "../database/dbConnection.js";
-import "../config/cloudinary.js"; // Assuming you made cloudinary config reusable
-
-dbConnection(); // Connect to MongoDB
-
-export default serverless(app);
